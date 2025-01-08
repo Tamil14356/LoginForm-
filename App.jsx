779f66ff34dded18fb661f4f36021b12f5911663
Tamil14356 <mtamilarasan56143@gmail.com>
2025-01-07 23:32:39 -0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,26 +1,3 @@
-// import React from 'react'
-// import Home from './Project9AxiosCard/Components/Home'
-// import Create from './Project9AxiosCard/Create'
-// import Edit from './Project9AxiosCard/Edit'
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-// const App = () => {
-//   return (
-//     <div>
-//       <BrowserRouter>
-//         <Routes>
-//             <Route path='/' element={<Home />} />
-//             <Route path='/create' element={<Create />} />
-//             <Route path='/edit/:id' element={<Edit />} />
-//         </Routes>
-//         </BrowserRouter>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
 import React from 'react'
 import { Login } from './LoginForm/Login'
 const App = () => {
